79464cd5e513f6b438eb188fa8c13240afca6077
Matheus Reis <theus.h.ass.reis@gmail.com>
2020-02-03 20:17:39 -0300

    Configure models views

--- library_project/library_app/views.py
+++ library_project/library_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
+from rest_framework import viewsets
 
 # Create your views here.
+class AuthorAPI(viewsets.ModelViewSet):
+    queryset = Author.objects.all().order_by('name')
+    serializer_class = AuthorSerializer
+
+class BookAPI(viewsets.ModelViewSet):
+    queryset = Book.objects.all().order_by('id')\
+    serializer_class = BookSerializer
