34d7ecc9803bd534b975aa0cfd58fe21a8db138a
Matheus Reis <theus.h.ass.reis@gmail.com>
2020-02-03 19:58:19 -0300

    Made book and author models and created migrations, installed library app in project

--- library_project/library_app/models.py
+++ library_project/library_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    edition = models.FloatField()
+    publication_year = models.FloatField()
+    authors = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return self.name
