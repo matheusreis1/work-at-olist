1cf2aa0fe04325fa2ce63f2542f1a05ee5738fcf
Matheus Reis <theus.h.ass.reis@gmail.com>
2020-02-03 20:04:52 -0300

    Added serializers classes

---
+++ library_project/library_app/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Author, Book
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ('name')
+
+class BookSerializer(serializers.ModelSerializer):
+    authors = AuthorSerializer(many=True)
+
+    class Meta:
+        model = Book
+        fields = '__all__'
