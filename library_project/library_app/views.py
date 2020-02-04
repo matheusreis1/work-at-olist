1e028948b2cf37977cb608105402a53b6562b6e3
Matheus Reis <theus.h.ass.reis@gmail.com>
2020-02-04 09:58:59 -0300

    Start filtering logic

--- library_project/library_app/views.py
+++ library_project/library_app/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 # Create your views here.
 class AuthorAPI(viewsets.ModelViewSet):
     queryset = Author.objects.all().order_by('name')
     serializer_class = AuthorSerializer
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = ['name']
 
 class BookAPI(viewsets.ModelViewSet):
     queryset = Book.objects.all().order_by('id')
     serializer_class = BookSerializer
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = ['name', 'publication_year', 'edition']
