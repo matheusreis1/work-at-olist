11eb5274be09dfc8113f683d35335c9048d0a8bb
Matheus Reis <theus.h.ass.reis@gmail.com>
2020-02-05 13:49:33 -0300

    Verfication of csv file in import authors command

--- library_project/library_app/management/commands/import_authors.py
+++ library_project/library_app/management/commands/import_authors.py
@@ -10,12 +10,22 @@ class Command(BaseCommand):
         parser.add_argument('csv_file')
 
     def handle(self, *args, **options):
+        self.stdout.write('Starting import authors')
         csv_file = options.get('csv_file')
+
+        try:
+            open(csv_file)
+        except OSError:
+            self.stderr.write("Could not open/read file: "+options.get('csv_file'))
+            return
 
         with open(csv_file, newline='') as csvfile:
             spamreader = csv.reader(csvfile, delimiter=',')
+            headers = next(spamreader)
             for row in spamreader:
                 name = row[0]
                 Author.objects.create(
                   name=name
                 )
+
+        self.stdout.write('Done')
