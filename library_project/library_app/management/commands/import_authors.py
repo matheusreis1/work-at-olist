8884c5ce234627d213c5c865eb529b970d7d08e8
Matheus Reis <theus.h.ass.reis@gmail.com>
2020-02-04 23:17:30 -0300

    Created command to import authors of csv file

---
+++ library_project/library_app/management/commands/import_authors.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand, CommandError
+from library_app.models import Author
+import csv
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('csv_file')
+
+    def handle(self, *args, **options):
+        csv_file = options.get('csv_file')
+
+        with open(csv_file, newline='') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+            for row in spamreader:
+                name = row[0]
+                Author.objects.create(
+                  name=name
+                )
