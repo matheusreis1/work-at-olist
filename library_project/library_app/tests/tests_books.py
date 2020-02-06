9cc7d8b84834efbe68935948b317e5f89a48fabe
Matheus Reis <theus.h.ass.reis@gmail.com>
2020-02-06 18:01:17 -0300

    More tests in books api

--- library_project/library_app/tests/tests_books.py
+++ library_project/library_app/tests/tests_books.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
 from library_app.models import Author, Book
+from library_app.serializers import AuthorSerializer, BookSerializer
 from library_project.settings import REST_FRAMEWORK as drf_configs
 import json
 
@@ -71,9 +72,6 @@ class BookApiTest(TestCase):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
 
-    # CRUD
-    # Search by name, publication_year, edition, author
-
     def test_count_get_books(self):
         """
             Test of count of books in GET method
@@ -83,6 +81,134 @@ class BookApiTest(TestCase):
         count = content['count']
         self.assertEqual(response.status_code, 200)
         self.assertEqual(count, Book.objects.all().count())
+
+    def test_post_right_book(self):
+        """
+            Test to create a book with right fields and values
+        """
+        json_book = {
+            "name": "Book Test",
+            "edition": 1,
+            "publication_year": 2020,
+            "authors": [1]
+        }
+        response = self.client.post(self.url, data=json_book)
+        content = json.loads(response.content)
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue('id' in content.keys())
+        self.assertTrue(content['name'] == json_book['name'])
+        self.assertTrue(content['edition'] == json_book['edition'])
+        self.assertTrue(content['publication_year'] == json_book['publication_year'])
+        self.assertTrue(content['authors'] == json_book['authors'])
+
+    def test_post_book_negative_edition(self):
+        """
+            Test create a book with right fields and
+            negative value in edition
+        """
+        json_book = {
+            "name": "Book Test",
+            "edition": -1,
+            "publication_year": 2020,
+            "authors": [1]
+        }
+        response = self.client.post(self.url, data=json_book)
+        content = json.loads(response.content)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(content['edition'], ["edition must be a positive integer."])
+
+    def test_post_book_negative_year(self):
+        """
+            Test create a book with right fields and
+            negative value in publication year
+        """
+        json_book = {
+            "name": "Book Test",
+            "edition": 1,
+            "publication_year": -2020,
+            "authors": [1]
+        }
+        response = self.client.post(self.url, data=json_book)
+        content = json.loads(response.content)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(content['publication_year'], ["publication_year must be a positive integer."])
+
+    def test_post_book_negative_edition_and_year(self):
+        """
+            Test to create a book with right fields and 
+            negative values in edition and publication year
+        """
+        json_book = {
+            "name": "Book Test",
+            "edition": -1,
+            "publication_year": -2020,
+            "authors": [1]
+        }
+        response = self.client.post(self.url, data=json_book)
+        content = json.loads(response.content)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(content['publication_year'], ["publication_year must be a positive integer."])
+        self.assertEqual(content['edition'], ["edition must be a positive integer."])
+
+    def test_create_book_without_name(self):
+        """
+            Test creation of book without name
+        """
+        json_book = {
+            "name": "",
+            "edition": 1,
+            "publication_year": 2020,
+            "authors": [1]
+        }
+        response = self.client.post(self.url, data=json_book)
+        content = json.loads(response.content)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(content['name'], ["This field may not be blank."])
+
+    def test_create_book_without_edition(self):
+        """
+            Test creation of book without edition
+        """
+        json_book = {
+            "name": "Book test",
+            "edition": "",
+            "publication_year": 2020,
+            "authors": [1]
+        }
+        response = self.client.post(self.url, data=json_book)
+        content = json.loads(response.content)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(content['edition'], ["A valid integer is required."])
+
+    def test_create_book_without_year(self):
+        """
+            Test creation of book without publication year
+        """
+        json_book = {
+            "name": "Book test",
+            "edition": 1,
+            "publication_year": "",
+            "authors": [1]
+        }
+        response = self.client.post(self.url, data=json_book)
+        content = json.loads(response.content)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(content['publication_year'], ["A valid integer is required."])
+
+    def test_create_book_without_authors(self):
+        """
+            Test creation of book without authors list
+        """
+        json_book = {
+            "name": "Book test",
+            "edition": 1,
+            "publication_year": 2020,
+            "authors": []
+        }
+        response = self.client.post(self.url, data=json_book)
+        content = json.loads(response.content)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(content['authors'], ["This list may not be empty."])
 
     def test_search_exact_name(self):
         """
@@ -136,9 +262,77 @@ class BookApiTest(TestCase):
         content = json.loads(response.content)
         count = content['count']
         self.assertEqual(response.status_code, 200)
-    
-    # def test_(self):
-    #     pass
-    
-    # def test_(self):
-    #     pass
+
+    def test_put_book(self):
+        """
+            Test to update book's fields
+        """
+        json_book = {
+            "name": "Book 11",
+            "edition": 2,
+            "publication_year": 2021,
+            "authors": [1, 2]
+        }
+        response = self.client.put(self.url+"1/", data=json_book)
+        content = json.loads(response.content)
+        book_db = Book.objects.get(id=1)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(content, BookSerializer(book_db).data)
+
+    def test_put_book_negative_edition(self):
+        """
+            Test to update book with a negative edition
+        """
+        json_book = {
+            "name": "Book 11",
+            "edition": -2,
+            "publication_year": 2021,
+            "authors": [1, 2]
+        }
+        response = self.client.put(self.url+"1/", data=json_book)
+        content = json.loads(response.content)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(content['edition'], ["edition must be a positive integer."])
+
+    def test_put_book_negative_year(self):
+        """
+            Test to update book with a negative publication year
+        """
+        json_book = {
+            "name": "Book 11",
+            "edition": -2,
+            "publication_year": -2021,
+            "authors": [1, 2]
+        }
+        response = self.client.put(self.url+"1/", data=json_book)
+        content = json.loads(response.content)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(content['edition'], ["edition must be a positive integer."])
+        self.assertEqual(content['publication_year'], ["publication_year must be a positive integer."])
+
+    def test_put_book_negative_year_and_edition(self):
+        """
+            Test to update book with negative publication year
+            and edition
+        """
+        json_book = {
+            "name": "Book 11",
+            "edition": -2,
+            "publication_year": -2021,
+            "authors": [1, 2]
+        }
+        response = self.client.put(self.url+"1/", data=json_book)
+        content = json.loads(response.content)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(content['publication_year'], ["publication_year must be a positive integer."])
+
+    def test_delete_book(self):
+        """
+            Test to delete a book
+        """
+        response = self.client.delete(self.url+"1/")
+        self.assertEqual(response.status_code, 204)
+        response_get = self.client.get(self.url+"1/")
+        content = json.loads(response_get.content)
+        self.assertEqual(response_get.status_code, 404)
+        self.assertEqual(content, {"detail": "Not found."})
