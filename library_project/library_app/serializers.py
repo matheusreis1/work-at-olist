719506a8212992ff9ddb8cbb98bd1c12d812ee6d
Matheus Reis <theus.h.ass.reis@gmail.com>
2020-02-05 22:08:20 -0300

    Validation of publication year and edition in books serializer

--- library_project/library_app/serializers.py
+++ library_project/library_app/serializers.py
@@ -10,3 +10,20 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+
+    def validate(self, data):
+        """
+            Validate values of publication year and edition in books api
+        """
+        publication_year = data.get("publication_year", None)
+        edition = data.get("edition", None)
+        messages = {}
+        if publication_year and publication_year <= 0:
+            messages['publication_year'] = "publication_year must be a positive integer."
+
+        if edition and edition <= 0:
+            messages['edition'] = "edition must be a positive integer."
+
+        if messages:
+            raise serializers.ValidationError(messages)
+        return data
